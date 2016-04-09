02c74fdd84f4158526db1bec84cc9c28719a1c7f
Szabó Gergő <gergo85@gmail.com>
2016-04-09 19:40:48 +0200

    Improved the multilingual support

---
+++ lang/hu/lang.php
@@ -0,0 +1,12 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Csempék',
+        'description' => 'A bővítmény listák megjelenítése csempe-szerűen.'
+    ],
+    'list' => [
+        'check' => 'Kiválasztás',
+        'edit' => 'szerkesztés',
+        'delete' => 'törlés',
+        'upload' => 'Feltöltés'
+    ]
+];
