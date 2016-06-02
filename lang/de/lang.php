d65a4a66669c82c13b4d6cd0ecc94de258be559c
Sewa Golubkow <s.golubkow@gmail.com>
2016-06-02 14:10:31 +0200

    added 2 languages

---
+++ lang/de/lang.php
@@ -0,0 +1,12 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Tileable',
+        'description' => 'Kachel-Anzeige von Listen und Relationen im Backend'
+    ],
+    'list' => [
+        'check' => 'Anw&auml;hlen',
+        'edit' => 'Bearbeiten',
+        'delete' => 'L&ouml;schen',
+        'upload' => 'Raufladen &amp; Erstellen'
+    ]
+];
